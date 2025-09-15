c005bd2f2be83d04e92337e833ad0125eef33599
Ikasle Arangoya <ikasle@arangoya.local>
2025-09-15 08:37:27 +0200

    First commit

---
+++ Tema-1/Ejercicios Tema 1/E2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class E2 {
+    public static void main(String[] args) {
+        System.out.print("Ingrese un numero: ");
+
+        Scanner scanner = new Scanner(System.in);
+        int numero_1 = scanner.nextInt();
+
+        System.out.print("Ingrese un segundo: ");
+        int numero_2 = scanner.nextInt();
+
+        int suma = numero_1 + numero_2;
+        int resta = numero_1 - numero_2;
+        int multi = numero_1 * numero_2;
+        double division = (double) numero_1 / numero_2;
+
+        System.out.println("El resultado de la suma es: " + suma);
+        System.out.println("El resultado de la resta es: " + resta);
+        System.out.println("El resultado de la multiplicación es: " + multi);
+        System.out.println("El resultado de la division es: " + division);
+
+
+        scanner.close();
+    }
+}
---
+++ Tema-1/Ejercicios Tema 1/E6.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class E6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        final double IVA = 0.21;
+
+        System.out.print("Ingrese el precio final del producto: ");
+        double total = scanner.nextDouble();
+        
+        double impuestos = total * IVA;
+
+        System.out.println("El suplemento cobrado por IVA es de : " + impuestos + " euros");
+    }
+}
